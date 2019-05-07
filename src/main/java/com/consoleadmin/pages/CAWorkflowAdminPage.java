b35bfe6a298e8c1d79804103d2a6a3ceb05d305c
Roy Alcantara <Roy.Alcantara@melbourneit.com.au>
2019-05-07 13:01:04 +1000

    update regression files

--- src/main/java/com/consoleadmin/pages/CAWorkflowAdminPage.java
+++ src/main/java/com/consoleadmin/pages/CAWorkflowAdminPage.java
@@ -89,7 +89,7 @@ public class CAWorkflowAdminPage extends TestBase{
     	}
     	
     	System.out.println("Execute Action button clicked");
-    	Thread.sleep(50000);			
+    	Thread.sleep(80000);			
     }
     
     
