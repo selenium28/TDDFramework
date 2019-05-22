e2018da497330139f48ff6fa567c4bbc25952f83
Roy Alcantara <Roy.Alcantara@melbourneit.com.au>
2019-05-22 13:39:20 +1000

    update test cases

--- src/main/java/com/consoleadmin/pages/CAWorkflowAdminPage.java
+++ src/main/java/com/consoleadmin/pages/CAWorkflowAdminPage.java
@@ -89,7 +89,7 @@ public class CAWorkflowAdminPage extends TestBase{
     	}
     	
     	System.out.println("Execute Action button clicked");
-    	Thread.sleep(60000);			
+    	Thread.sleep(30000);			
     }
     
     
@@ -114,7 +114,7 @@ public class CAWorkflowAdminPage extends TestBase{
     	}
     	
     	System.out.println("Execute Action button clicked");
-    	Thread.sleep(40000);			
+    	Thread.sleep(30000);			
     }
     
     
