30ad6b5d3f78947606f36f332ba4ef67d9bbecd7
Roy Alcantara <Roy.Alcantara@melbourneit.com.au>
2019-07-08 16:48:45 +1000

    enable old shopping cart order only

--- src/test/java/com/consoleregression/workflowtest/testcases/TestDomainRegistration2WorkflowForCom.java
+++ src/test/java/com/consoleregression/workflowtest/testcases/TestDomainRegistration2WorkflowForCom.java
@@ -101,17 +101,17 @@ public class TestDomainRegistration2WorkflowForCom extends TestBase{
 		System.out.println("Reference ID[0]:" + strWorkflowId);	
 		driver.close();
 		
-		//Test Step 4: Process the domain registration order in console admin
-		initialization(environment, "consoleadmin");
-		caloginpage = new CALoginPage();
-		caheaderpage = caloginpage.setDefaultLoginDetails(environment);
-		caworkflowadminpage = caheaderpage.searchWorkflow(strWorkflowId);
-		caworkflowadminpage.processDomainRegistrationWF(strWorkflowId);
-		caworkflowadminpage.processFraudCheck();
-		
-		//Test Step 5: Verify if domain registration workflow is completed
-		caworkflowadminpage = caheaderpage.searchWorkflow(strWorkflowId);
-		Assert.assertEquals(caworkflowadminpage.getWorkflowStatus("domainregistration2"), "domain registration completed", caworkflowadminpage.getWorkflowStatus("domainregistration2"));
+//		//Test Step 4: Process the domain registration order in console admin
+//		initialization(environment, "consoleadmin");
+//		caloginpage = new CALoginPage();
+//		caheaderpage = caloginpage.setDefaultLoginDetails(environment);
+//		caworkflowadminpage = caheaderpage.searchWorkflow(strWorkflowId);
+//		caworkflowadminpage.processDomainRegistrationWF(strWorkflowId);
+//		caworkflowadminpage.processFraudCheck();
+//		
+//		//Test Step 5: Verify if domain registration workflow is completed
+//		caworkflowadminpage = caheaderpage.searchWorkflow(strWorkflowId);
+//		Assert.assertEquals(caworkflowadminpage.getWorkflowStatus("domainregistration2"), "domain registration completed", caworkflowadminpage.getWorkflowStatus("domainregistration2"));
 		
 		driver.close();
 		System.out.println("End Test: testDomainRegistration2WorkflowForCom");
