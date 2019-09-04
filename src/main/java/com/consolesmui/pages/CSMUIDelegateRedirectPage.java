b28454d7fca3c0c5fd6c13aef21c14be85f4d573
Jet Palma <jet.palma@arq.group>
2019-09-04 12:03:36 +1000

    Updated CSMUITabPage and created 5 new pages

---
+++ src/main/java/com/consolesmui/pages/CSMUIDelegateRedirectPage.java
@@ -0,0 +1,7 @@
+package com.consolesmui.pages;
+
+import com.base.TestBase;
+
+public class CSMUIDelegateRedirectPage extends TestBase {
+
+}
--- src/main/java/com/consolesmui/pages/CSMUITabPage.java
+++ src/main/java/com/consolesmui/pages/CSMUITabPage.java
@@ -1,11 +1,73 @@
 package com.consolesmui.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
+
 import com.base.TestBase;
 
 public class CSMUITabPage extends TestBase {
 
+	// Objects
+	@FindBy(how = How.CSS, using = "a[href*=\"dashboard\"]")
+	WebElement dashboardTab;
+
+	@FindBy(how = How.ID, using = "domainname-tab-link")
+	WebElement domainNameTab;
+
+	@FindBy(how = How.ID, using = "zonemanager-tab-link")
+	WebElement zoneManagerTab;
+
+	@FindBy(how = How.ID, using = "delegate-domain-tab-link")
+	WebElement delegateDomainTab;
+
+	@FindBy(how = How.ID, using = "domain-redirector-tab-link")
+	WebElement delegateRedirectorTab;
+
+	@FindBy(how = How.ID, using = "o365-tab-link")
+	WebElement office365Tab;
+
+	// Initializing Page Objects
 	public CSMUITabPage() {
-		// TODO Auto-generated constructor stub
+		PageFactory.initElements(driver, this);
+	}
+
+	// Methods
+	public CSMUIDashboardPage clickDashoardTab() {
+
+		dashboardTab.click();
+		return new CSMUIDashboardPage();
+	}
+
+	public CSMUIDomainNamePage clickDomainNameTab() {
+
+		domainNameTab.click();
+		return new CSMUIDomainNamePage();
+	}
+
+	public CSMUIZoneManagerPage clickZoneManagerTab() {
+
+		zoneManagerTab.click();
+		return new CSMUIZoneManagerPage();
+	}
+
+	public CSMUIDelegateDomainPage clickDelegateDomainTab() {
+
+		delegateDomainTab.click();
+		return new CSMUIDelegateDomainPage();
+	}
+
+	public CSMUIDelegateRedirectPage clickDelegateRedirectorTab() {
+
+		delegateRedirectorTab.click();
+		return new CSMUIDelegateRedirectPage();
+	}
+
+	public CSMUIManageO365MainPage clickOffice365Tab() {
+
+		office365Tab.click();
+		return new CSMUIManageO365MainPage();
 	}
 
 }
