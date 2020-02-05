0a5e9d3a341024ec759e017319ee0de54976a203
Phani Anepalli <phani.anepalli@arq.group>
2020-02-05 14:32:38 +1100

    checkdomainavailability for customer portal

---
+++ src/main/java/com/tppcustomerportal/pages/TPPLoginPage.java
@@ -0,0 +1,11 @@
+package com.tppcustomerportal.pages;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
+
+import com.base.TestBase;
+
+public class TPPLoginPage extends TestBase {
+
+}
